6e54b7b72555c55f791b9d130bcb219cb4958289
jvoliveira7 <oliveirajoaovictor774@gmail.com>
2026-03-23 23:42:57 -0300

    Clonagem do repositório.

--- tests/livros.test.js
+++ tests/livros.test.js
@@ -1,8 +1,8 @@
-const request = require('supertest');
-const app = require('../src/app');
+        const request = require('supertest');
+        const app = require('../src/app');
 
-test('POST /livros cria um livro', async () => {
-    const res = await request(app).post('/livros').send({ titulo: 'Clean Code', autor: 'Martin Code'});
-    expect(res.status).toBe(201);
-    expect(res.body.titulo).toBe('Clean Code');
-});
+        test('POST /livros cria um livro', async () => {
+            const res = await request(app).post('/livros').send({ titulo: 'Clean Code', autor: 'Martin Code'});
+            expect(res.status).toBe(201);
+            expect(res.body.titulo).toBe('Clean Code');
+        });
